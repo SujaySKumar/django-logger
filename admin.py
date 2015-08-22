c539727cade3f977168f6ac5fc166024a5da79ff
SujaySKumar <sujay.skumar141295@gmail.com>
2015-08-22 16:32:28 +0530

    Naive implementation of logging

--- admin.py
+++ admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class SujayLoggerAdmin(admin.ModelAdmin):
+    def log_addition(self, request, object, message):
+        from django.contrib.admin.models import LogEntry, ADDITION
+        LogEntry.objects.log_action(
+            user_id=request.user.pk,
+            content_type_id=get_content_type_for_model(object).pk,
+            object_id=object.pk,
+            object_repr=force_text(object),
+            action_flag=ADDITION,
+            change_message="Object has been added",
+        )
